2a4f7dd773619fc4731fe51180b202e976c687a8
kaminyv <kaminyv@yandex.ru>
2023-02-21 22:19:27 +0600

    Added docker configuration.

---
+++ mailganer/mailganer/celery.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mailganer.settings')
+
+app = Celery('mailganer')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
--- mailganer/mailganer/settings.py
+++ mailganer/mailganer/settings.py
@@ -128,4 +128,7 @@ STATIC_URL = '/static/'
 
 # Django debug tool
 INTERNAL_IPS = os.environ.get('INTERNAL_IPS').split(' ')
-# INTERNAL_IPS = ['127.0.0.1']
+if DEBUG:
+    import socket  # only if you haven't already imported this
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
