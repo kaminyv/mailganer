6f5a3d0e2c16af2f7c98636418c204e2405a3da1
kaminyv <kaminyv@yandex.ru>
2023-02-22 10:07:00 +0600

    Update docker compose configuration.

--- mailganer/mailganer/settings.py
+++ mailganer/mailganer/settings.py
@@ -132,3 +132,11 @@ if DEBUG:
     import socket  # only if you haven't already imported this
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+
+# Email settings
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = int(os.environ.get('EMAIL_USE_TLS'))
